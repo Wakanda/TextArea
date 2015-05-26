9d41564ed405d90b8ce7c779dad5b276753b8ef5
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2015-05-26 11:28:40 +0200

    code quality improvements

--- widget.js
+++ widget.js
@@ -1,41 +1,6 @@
 WAF.define('TextArea', ['waf-core/widget'], function(widget) {
     'use strict';
 
-    function updateAttribute(property, valueOverwrite) {
-        var value = this[property]();
-
-        if (value) {
-            this.node.setAttribute(property.toLowerCase(), valueOverwrite || value);
-        }
-        else {
-            this.node.removeAttribute(property.toLowerCase());
-        }
-    }
-
-    function onChange(event) {
-        var value = event.data.value;
-
-        switch (event.target) {
-            case 'value':
-                this.node.value = value;
-                this.node.textContent = value;
-                return;
-
-            case 'maxLength':
-                if (value < 0) {
-                    this.maxLength(0);
-                    value = 0;
-                }
-                break;
-
-            case 'readOnly':
-                value = (value && 'readonly');
-                break;
-        }
-
-        updateAttribute.call(this, event.target, value);
-    }
-
     var TextArea = widget.create('TextArea', {
         tagName: 'textarea',
         value: widget.property({
@@ -76,14 +41,14 @@ WAF.define('TextArea', ['waf-core/widget'], function(widget) {
 
             this.addClass('form-control');
 
-            updateAttribute.call(this, 'readOnly', 'readonly');
-            updateAttribute.call(this, 'maxLength');
-            updateAttribute.call(this, 'placeholder');
+            this.updateAttribute.call(this, 'readOnly', this.readOnly());
+            this.updateAttribute.call(this, 'maxLength', this.maxLength());
+            this.updateAttribute.call(this, 'placeholder', this.placeholder());
 
             this.node.textContent = this.value();
             this.node.value = this.value();
 
-            subscriber = this.subscribe('change', onChange, this);
+            subscriber = this.subscribe('change', this.onChange, this);
             subscriber.options.once = true;
 
             this.node.addEventListener('change', function() {
@@ -92,6 +57,37 @@ WAF.define('TextArea', ['waf-core/widget'], function(widget) {
         },
         hasFocus: function hasFocus () {
             return (document.activeElement === this.node);
+        },
+        updateAttribute: function updateAttribute(property, valueOverwrite) {
+            var value = this[property]();
+            if (valueOverwrite === 0 || valueOverwrite) {
+                this.node.setAttribute(property.toLowerCase(), String(valueOverwrite));
+            }
+            else {
+                this.node.removeAttribute(property.toLowerCase());
+            }
+        },
+        onChange: function onChange(event) {
+            var value = event.data.value;
+
+            switch (event.target) {
+                case 'value':
+                    this.node.value = value;
+                    this.node.textContent = value;
+                    return;
+
+                case 'maxLength':
+                    if (parseInt(value) < 0) {
+                        value = 0;
+                    }
+                    break;
+
+                case 'readOnly':
+                    value = (value && 'readonly');
+                    break;
+            }
+
+            this.updateAttribute.call(this, event.target, value);
         },
         disable: function() {
            this.$super('disable')();
